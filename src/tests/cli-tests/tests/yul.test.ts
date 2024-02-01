6fbc7ecd768cc73764917ac53ec3ce6c49524697
pcheremu <pavel.cheremushkin@gmail.com>
2024-02-01 01:03:07 +0100

    test: added yul tests

--- src/tests/cli-tests/tests/yul.test.ts
+++ src/tests/cli-tests/tests/yul.test.ts
@@ -19,8 +19,8 @@ describe("Run with --yul by default", () => {
   });
 
 
-  xit("solc exit code == zksolc exit code", () => { // unknown solc issue for datatype of the contract
-      const command = `solc ${paths.pathToBasicSolContract} --yul`;
+  xit("solc exit code == zksolc exit code", () => { // issue with solc compilation
+      const command = `solc ${paths.pathToBasicYulContract} --yul`;
       const solcResult = executeCommand(command);
       expect(solcResult.exitCode).toBe(result.exitCode);
   });
@@ -32,9 +32,91 @@ describe("Run with --yul by default", () => {
     expect(resultInvalid.exitCode).toBe(1);
   });
 
-  it("Invalid solc exit code == Invalid zksolc exit code", () => {
+  it("Invalid solc exit code == Invalid zksolc exit code", () => { 
     const command = 'solc --yul';
     const solcResult = executeCommand(command);
     expect(solcResult.exitCode).toBe(resultInvalid.exitCode);
   });
 });
+
+//id1820
+describe("Run with double yul options", () => {
+  const command = `zksolc ${paths.pathToBasicYulContract} --yul --yul`;
+  const result = executeCommand(command);
+
+  it("Valid command exit code = 1", () => {
+    expect(result.exitCode).toBe(1);
+  });
+
+  it("--yul error is presented", () => {
+    expect(result.output).toMatch(/(The argument '--yul' was provided more than once,)/i);
+  });
+
+  it("solc exit code == zksolc exit code", () => {
+      const command = `solc ${paths.pathToBasicYulContract} --yul --yul`;
+      const solcResult = executeCommand(command);
+      expect(solcResult.exitCode).toBe(result.exitCode);
+  });
+});
+
+//id1821
+describe("Run with incompatible input format", () => {
+  const command = `zksolc ${paths.pathToBasicSolContract} --yul`;
+  const result = executeCommand(command);
+
+  it("Valid command exit code = 1", () => {
+    expect(result.exitCode).toBe(1);
+  });
+
+  it("--yul error is presented", () => {
+    expect(result.output).toMatch(/(Error: Expected keyword "object")/i);
+  });
+
+  it("solc exit code == zksolc exit code", () => {
+      const command = `solc ${paths.pathToBasicSolContract} --yul`;
+      const solcResult = executeCommand(command);
+      expect(solcResult.exitCode).toBe(result.exitCode);
+  });
+});
+
+//id1819:1
+describe("Run with incompatible json modes --combined-json", () => {
+  const command = `zksolc ${paths.pathToBasicSolContract} --yul --combined-json anyarg`;
+  const result = executeCommand(command);
+
+  it("Valid command exit code = 1", () => {
+    expect(result.exitCode).toBe(1);
+  });
+
+  it("--yul error is presented", () => {
+    expect(result.output).toMatch(/(Only one modes is allowed at the same time:)/i);
+  });
+
+  it("solc exit code == zksolc exit code", () => {
+      const command = `solc ${paths.pathToBasicSolContract} --yul --combined-json anyarg`;
+      const solcResult = executeCommand(command);
+      expect(solcResult.exitCode).toBe(result.exitCode);
+  });
+});
+
+//id1819:2
+describe("Run with incompatible json modes --standard-json", () => {
+  const command = `zksolc ${paths.pathToBasicSolContract} --yul --standard-json`;
+  const result = executeCommand(command);
+
+  it("Valid command exit code = 1", () => {
+    expect(result.exitCode).toBe(1);
+  });
+
+  it("--yul error is presented", () => {
+    expect(result.output).toMatch(/(Only one modes is allowed at the same time:)/i);
+  });
+
+  it("solc exit code == zksolc exit code", () => {
+      const command = `solc ${paths.pathToBasicSolContract} --yul --standard-json`;
+      const solcResult = executeCommand(command);
+      expect(solcResult.exitCode).toBe(result.exitCode);
+  });
+});
+
+
