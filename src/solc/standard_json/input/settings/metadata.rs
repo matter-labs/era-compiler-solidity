b1c197c7d374cbe98d1f292d1a8ba4bc9e904d3f
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-04-23 21:10:55 +0700

    Support the useLiteralContent metadata flag (#64)

--- src/solc/standard_json/input/settings/metadata.rs
+++ src/solc/standard_json/input/settings/metadata.rs
@@ -14,15 +14,22 @@ pub struct Metadata {
     /// The bytecode hash mode.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub bytecode_hash: Option<era_compiler_llvm_context::EraVMMetadataHash>,
+    /// Whether to use literal content.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub use_literal_content: Option<bool>,
 }
 
 impl Metadata {
     ///
     /// A shortcut constructor.
     ///
-    pub fn new(bytecode_hash: era_compiler_llvm_context::EraVMMetadataHash) -> Self {
+    pub fn new(
+        bytecode_hash: era_compiler_llvm_context::EraVMMetadataHash,
+        use_literal_content: bool,
+    ) -> Self {
         Self {
             bytecode_hash: Some(bytecode_hash),
+            use_literal_content: Some(use_literal_content),
         }
     }
 }
