ccd21b2d2ce951266c020cc58cc51ede02980200
Igor Zhirkov <iz@matterlabs.dev>
2024-05-13 00:11:31 +0200

    [ec] add is_* methods for statements

--- src/easycrypt/syntax/statement/mod.rs
+++ src/easycrypt/syntax/statement/mod.rs
@@ -19,3 +19,69 @@ pub enum Statement {
     // SAssignment for // x <$ distr
     Pass,
 }
+
+impl Statement {
+    /// Returns `true` if the statement is [`VarDefinition`].
+    ///
+    /// [`VarDefinition`]: Statement::VarDefinition
+    #[must_use]
+    pub fn is_var_definition(&self) -> bool {
+        matches!(self, Self::VarDefinition(..))
+    }
+
+    /// Returns `true` if the statement is [`Expression`].
+    ///
+    /// [`Expression`]: Statement::Expression
+    #[must_use]
+    pub fn is_expression(&self) -> bool {
+        matches!(self, Self::Expression(..))
+    }
+
+    /// Returns `true` if the statement is [`Block`].
+    ///
+    /// [`Block`]: Statement::Block
+    #[must_use]
+    pub fn is_block(&self) -> bool {
+        matches!(self, Self::Block(..))
+    }
+
+    /// Returns `true` if the statement is [`If`].
+    ///
+    /// [`If`]: Statement::If
+    #[must_use]
+    pub fn is_if(&self) -> bool {
+        matches!(self, Self::If(..))
+    }
+
+    /// Returns `true` if the statement is [`EAssignment`].
+    ///
+    /// [`EAssignment`]: Statement::EAssignment
+    #[must_use]
+    pub fn is_eassignment(&self) -> bool {
+        matches!(self, Self::EAssignment(..))
+    }
+
+    /// Returns `true` if the statement is [`PAssignment`].
+    ///
+    /// [`PAssignment`]: Statement::PAssignment
+    #[must_use]
+    pub fn is_passignment(&self) -> bool {
+        matches!(self, Self::PAssignment(..))
+    }
+
+    /// Returns `true` if the statement is [`Return`].
+    ///
+    /// [`Return`]: Statement::Return
+    #[must_use]
+    pub fn is_return(&self) -> bool {
+        matches!(self, Self::Return(..))
+    }
+
+    /// Returns `true` if the statement is [`While`].
+    ///
+    /// [`While`]: Statement::While
+    #[must_use]
+    pub fn is_while(&self) -> bool {
+        matches!(self, Self::While(..))
+    }
+}
