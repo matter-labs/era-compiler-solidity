4a4206cc358220e5f5db74968c8b07232b22313b
Igor Zhirkov <iz@matterlabs.dev>
2024-05-19 14:43:28 +0200

    [ec] derive EasyCrypt type from function signature

--- src/easycrypt/syntax/signature.rs
+++ src/easycrypt/syntax/signature.rs
@@ -21,6 +21,24 @@ pub struct Signature {
 }
 
 impl Signature {
+    /// Returns true if the signature return type is Unit.
+    pub fn returns_unit(&self) -> bool {
+        self.return_type == Type::Unit
+    }
+
+    /// Returns an instance of the arrow type matching the signature.
+    pub fn get_type(&self) -> Type {
+        Type::Arrow(
+            Box::from(Type::Tuple(
+                self.formal_parameters
+                    .iter()
+                    .map(|(_, typ)| typ)
+                    .cloned()
+                    .collect(),
+            )),
+            Box::from(self.return_type.clone()),
+        )
+    }
     /// A signature of a procedure with zero arguments.
     pub const UNIT_TO_UNIT: Signature = Signature {
         formal_parameters: vec![],
