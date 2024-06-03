fc782aecede9b10efb7f281d75b392cc9966a006
Igor Zhirkov <iz@matterlabs.dev>
2024-06-04 00:27:23 +0200

    encode usage attributes as a powerset

--- src/easycrypt/translator/definition_info/usage.rs
+++ src/easycrypt/translator/definition_info/usage.rs
@@ -5,11 +5,39 @@
 #[allow(dead_code)]
 /// How is memory or storage affected by a procedure.
 #[derive(Clone, Debug, PartialEq, Eq, Default)]
-pub enum Usage {
-    #[default]
-    NoUse,
-    Read,
-    Meta,
-    Write,
-    ReadWrite,
+pub struct Usage {
+    pub read: bool,
+    pub write: bool,
+    pub meta: bool,
+}
+
+impl Usage {
+    pub const READ: Usage = Usage {
+        read: true,
+        write: false,
+        meta: false,
+    };
+    pub const WRITE: Usage = Usage {
+        read: false,
+        write: true,
+        meta: false,
+    };
+    pub const META: Usage = Usage {
+        read: false,
+        write: false,
+        meta: true,
+    };
+    pub const RW: Usage = Usage {
+        read: true,
+        write: true,
+        meta: false,
+    };
+
+    pub fn union(&self, other: &Self) -> Self {
+        Self {
+            read: self.read || other.read,
+            write: self.write || other.write,
+            meta: self.meta || other.meta,
+        }
+    }
 }
