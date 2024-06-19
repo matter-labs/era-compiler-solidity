d9737168c1cd44a7ad92a51dd7c35fc1dc7465a6
Igor Zhirkov <iz@matterlabs.dev>
2024-06-19 15:07:45 +0200

    [refac] extract yul name string representation

--- src/yul/printer.rs
+++ src/yul/printer.rs
@@ -95,168 +95,7 @@ where
     }
 
     fn visit_name(&mut self, name: &Name) {
-        if let Name::Verbatim {
-            input_size,
-            output_size,
-        } = name
-        {
-            self.print(format!("verbatim_{input_size}i_{output_size}o").as_str())
-        } else {
-            self.print(match name {
-                Name::Add => "add",
-                Name::Sub => "sub",
-                Name::Mul => "mul",
-                Name::Div => "div",
-                Name::Mod => "mod",
-                Name::Sdiv => "sdiv",
-                Name::Smod => "smod",
-
-                Name::Lt => "lt",
-                Name::Gt => "gt",
-                Name::Eq => "eq",
-                Name::IsZero => "iszero",
-                Name::Slt => "slt",
-                Name::Sgt => "sgt",
-
-                Name::Or => "or",
-                Name::Xor => "xor",
-                Name::Not => "not",
-                Name::And => "and",
-                Name::Shl => "shl",
-                Name::Shr => "shr",
-                Name::Sar => "sar",
-                Name::Byte => "byte",
-                Name::Pop => "pop",
-
-                Name::AddMod => "addmod",
-                Name::MulMod => "mulmod",
-                Name::Exp => "exp",
-                Name::SignExtend => "signextend",
-
-                Name::Keccak256 => "keccak256",
-
-                Name::MLoad => "mload",
-                Name::MStore => "mstore",
-                Name::MStore8 => "mstore8",
-                Name::MCopy => "mcopy",
-
-                Name::SLoad => "sload",
-                Name::SStore => "sstore",
-                Name::TLoad => "tload",
-                Name::TStore => "tstore",
-                Name::LoadImmutable => "loadimmutable",
-                Name::SetImmutable => "setimmutable",
-
-                Name::CallDataLoad => "calldataload",
-                Name::CallDataSize => "calldatasize",
-                Name::CallDataCopy => "calldatacopy",
-                Name::CodeSize => "codesize",
-                Name::CodeCopy => "codecopy",
-                Name::ReturnDataSize => "returndatasize",
-                Name::ReturnDataCopy => "returndatacopy",
-                Name::ExtCodeSize => "extcodesize",
-                Name::ExtCodeHash => "extcodehash",
-
-                Name::Return => "return",
-                Name::Revert => "revert",
-
-                Name::Log0 => "log0",
-                Name::Log1 => "log1",
-                Name::Log2 => "log2",
-                Name::Log3 => "log3",
-                Name::Log4 => "log4",
-
-                Name::Call => "call",
-                Name::DelegateCall => "delegatecall",
-                Name::StaticCall => "staticcall",
-
-                Name::Create => "create",
-                Name::Create2 => "create2",
-                Name::ZkCreate => "$zk_create",
-                Name::ZkCreate2 => "$zk_create2",
-                Name::DataSize => "datasize",
-                Name::DataOffset => "dataoffset",
-                Name::DataCopy => "datacopy",
-
-                Name::Stop => "stop",
-                Name::Invalid => "invalid",
-
-                Name::LinkerSymbol => "linkersymbol",
-                Name::MemoryGuard => "memoryguard",
-
-                Name::Address => "address",
-                Name::Caller => "caller",
-
-                Name::CallValue => "callvalue",
-                Name::Gas => "gas",
-                Name::Balance => "balance",
-                Name::SelfBalance => "selfbalance",
-
-                Name::GasLimit => "gaslimit",
-                Name::GasPrice => "gasprice",
-                Name::Origin => "origin",
-                Name::ChainId => "chainid",
-                Name::Timestamp => "timestamp",
-                Name::Number => "number",
-                Name::BlockHash => "blockhash",
-                Name::BlobHash => "blobhash",
-                Name::Difficulty => "difficulty",
-                Name::Prevrandao => "prevrandao",
-                Name::CoinBase => "coinbase",
-                Name::BaseFee => "basefee",
-                Name::BlobBaseFee => "blobbasefee",
-                Name::MSize => "msize",
-
-                Name::CallCode => "callcode",
-                Name::Pc => "pc",
-                Name::ExtCodeCopy => "extcodecopy",
-                Name::SelfDestruct => "selfdestruct",
-
-                Name::ZkToL1 => "$zk_to_l1",
-                Name::ZkCodeSource => "$zk_code_source",
-                Name::ZkPrecompile => "$zk_precompile",
-                Name::ZkMeta => "$zk_meta",
-                Name::ZkSetContextU128 => "$zk_set_context_u128",
-                Name::ZkSetPubdataPrice => "$zk_set_pubdata_price",
-                Name::ZkIncrementTxCounter => "$zk_increment_tx_counter",
-                Name::ZkEventInitialize => "$zk_event_initialize",
-                Name::ZkEventWrite => "$zk_event_write",
-
-                Name::ZkMimicCall => "$zk_mimic_call",
-                Name::ZkSystemMimicCall => "$zk_system_mimic_call",
-                Name::ZkMimicCallByRef => "$zk_mimic_call_byref",
-                Name::ZkSystemMimicCallByRef => "$zk_system_mimic_call_byref",
-                Name::ZkRawCall => "$zk_raw_call",
-                Name::ZkRawCallByRef => "$zk_raw_call_byref",
-                Name::ZkSystemCall => "$zk_system_call",
-                Name::ZkSystemCallByRef => "$zk_system_call_byref",
-                Name::ZkStaticRawCall => "$zk_static_raw_call",
-                Name::ZkStaticRawCallByRef => "$zk_static_raw_call_byref",
-                Name::ZkStaticSystemCall => "$zk_static_system_call",
-                Name::ZkStaticSystemCallByRef => "$zk_static_system_call_byref",
-                Name::ZkDelegateRawCall => "$zk_delegate_raw_call",
-                Name::ZkDelegateRawCallByRef => "$zk_delegate_raw_call_byref",
-                Name::ZkDelegateSystemCall => "$zk_delegate_system_call",
-                Name::ZkDelegateSystemCallByRef => "$zk_delegate_system_call_byref",
-
-                Name::ZkLoadCalldataIntoActivePtr => "$zk_load_calldata_into_active_ptr",
-                Name::ZkLoadReturndataIntoActivePtr => "$zk_load_returndata_into_active_ptr",
-                Name::ZkPtrAddIntoActive => "$zk_ptr_add_into_active",
-                Name::ZkPtrShrinkIntoActive => "$zk_ptr_shrink_into_active",
-                Name::ZkPtrPackIntoActive => "$zk_ptr_pack_into_active",
-
-                Name::ZkMultiplicationHigh => "$zk_multiplication_high",
-
-                Name::ZkGlobalLoad => "$zk_global_load",
-                Name::ZkGlobalExtraAbiData => "$zk_global_extra_abi_data",
-                Name::ZkGlobalStore => "$zk_global_store",
-
-                Name::UserDefined(n) => n.as_str(),
-                Name::Verbatim { .. } => {
-                    unreachable!()
-                }
-            })
-        }
+        self.print(&name_identifier(name));
     }
 
     fn visit_function_call(&mut self, call: &FunctionCall) {
@@ -340,5 +179,172 @@ where
         }
         self.println(" }");
         self.decrease_indent();
+    }
+}
+
+/// Shows how an instance of [`Name`] is displayed in YUL code.
+pub fn name_identifier(name: &Name) -> String {
+    if let Name::Verbatim {
+        input_size,
+        output_size,
+    } = name
+    {
+        format!("verbatim_{input_size}i_{output_size}o")
+    } else if let Name::UserDefined(inner) = name {
+        inner.to_string()
+    } else {
+        String::from(match name {
+            Name::Add => "add",
+            Name::Sub => "sub",
+            Name::Mul => "mul",
+            Name::Div => "div",
+            Name::Mod => "mod",
+            Name::Sdiv => "sdiv",
+            Name::Smod => "smod",
+
+            Name::Lt => "lt",
+            Name::Gt => "gt",
+            Name::Eq => "eq",
+            Name::IsZero => "iszero",
+            Name::Slt => "slt",
+            Name::Sgt => "sgt",
+
+            Name::Or => "or",
+            Name::Xor => "xor",
+            Name::Not => "not",
+            Name::And => "and",
+            Name::Shl => "shl",
+            Name::Shr => "shr",
+            Name::Sar => "sar",
+            Name::Byte => "byte",
+            Name::Pop => "pop",
+
+            Name::AddMod => "addmod",
+            Name::MulMod => "mulmod",
+            Name::Exp => "exp",
+            Name::SignExtend => "signextend",
+
+            Name::Keccak256 => "keccak256",
+
+            Name::MLoad => "mload",
+            Name::MStore => "mstore",
+            Name::MStore8 => "mstore8",
+            Name::MCopy => "mcopy",
+
+            Name::SLoad => "sload",
+            Name::SStore => "sstore",
+            Name::TLoad => "tload",
+            Name::TStore => "tstore",
+            Name::LoadImmutable => "loadimmutable",
+            Name::SetImmutable => "setimmutable",
+
+            Name::CallDataLoad => "calldataload",
+            Name::CallDataSize => "calldatasize",
+            Name::CallDataCopy => "calldatacopy",
+            Name::CodeSize => "codesize",
+            Name::CodeCopy => "codecopy",
+            Name::ReturnDataSize => "returndatasize",
+            Name::ReturnDataCopy => "returndatacopy",
+            Name::ExtCodeSize => "extcodesize",
+            Name::ExtCodeHash => "extcodehash",
+
+            Name::Return => "return",
+            Name::Revert => "revert",
+
+            Name::Log0 => "log0",
+            Name::Log1 => "log1",
+            Name::Log2 => "log2",
+            Name::Log3 => "log3",
+            Name::Log4 => "log4",
+
+            Name::Call => "call",
+            Name::DelegateCall => "delegatecall",
+            Name::StaticCall => "staticcall",
+
+            Name::Create => "create",
+            Name::Create2 => "create2",
+            Name::ZkCreate => "$zk_create",
+            Name::ZkCreate2 => "$zk_create2",
+            Name::DataSize => "datasize",
+            Name::DataOffset => "dataoffset",
+            Name::DataCopy => "datacopy",
+
+            Name::Stop => "stop",
+            Name::Invalid => "invalid",
+
+            Name::LinkerSymbol => "linkersymbol",
+            Name::MemoryGuard => "memoryguard",
+
+            Name::Address => "address",
+            Name::Caller => "caller",
+
+            Name::CallValue => "callvalue",
+            Name::Gas => "gas",
+            Name::Balance => "balance",
+            Name::SelfBalance => "selfbalance",
+
+            Name::GasLimit => "gaslimit",
+            Name::GasPrice => "gasprice",
+            Name::Origin => "origin",
+            Name::ChainId => "chainid",
+            Name::Timestamp => "timestamp",
+            Name::Number => "number",
+            Name::BlockHash => "blockhash",
+            Name::BlobHash => "blobhash",
+            Name::Difficulty => "difficulty",
+            Name::Prevrandao => "prevrandao",
+            Name::CoinBase => "coinbase",
+            Name::BaseFee => "basefee",
+            Name::BlobBaseFee => "blobbasefee",
+            Name::MSize => "msize",
+
+            Name::CallCode => "callcode",
+            Name::Pc => "pc",
+            Name::ExtCodeCopy => "extcodecopy",
+            Name::SelfDestruct => "selfdestruct",
+
+            Name::ZkToL1 => "$zk_to_l1",
+            Name::ZkCodeSource => "$zk_code_source",
+            Name::ZkPrecompile => "$zk_precompile",
+            Name::ZkMeta => "$zk_meta",
+            Name::ZkSetContextU128 => "$zk_set_context_u128",
+            Name::ZkSetPubdataPrice => "$zk_set_pubdata_price",
+            Name::ZkIncrementTxCounter => "$zk_increment_tx_counter",
+            Name::ZkEventInitialize => "$zk_event_initialize",
+            Name::ZkEventWrite => "$zk_event_write",
+
+            Name::ZkMimicCall => "$zk_mimic_call",
+            Name::ZkSystemMimicCall => "$zk_system_mimic_call",
+            Name::ZkMimicCallByRef => "$zk_mimic_call_byref",
+            Name::ZkSystemMimicCallByRef => "$zk_system_mimic_call_byref",
+            Name::ZkRawCall => "$zk_raw_call",
+            Name::ZkRawCallByRef => "$zk_raw_call_byref",
+            Name::ZkSystemCall => "$zk_system_call",
+            Name::ZkSystemCallByRef => "$zk_system_call_byref",
+            Name::ZkStaticRawCall => "$zk_static_raw_call",
+            Name::ZkStaticRawCallByRef => "$zk_static_raw_call_byref",
+            Name::ZkStaticSystemCall => "$zk_static_system_call",
+            Name::ZkStaticSystemCallByRef => "$zk_static_system_call_byref",
+            Name::ZkDelegateRawCall => "$zk_delegate_raw_call",
+            Name::ZkDelegateRawCallByRef => "$zk_delegate_raw_call_byref",
+            Name::ZkDelegateSystemCall => "$zk_delegate_system_call",
+            Name::ZkDelegateSystemCallByRef => "$zk_delegate_system_call_byref",
+
+            Name::ZkLoadCalldataIntoActivePtr => "$zk_load_calldata_into_active_ptr",
+            Name::ZkLoadReturndataIntoActivePtr => "$zk_load_returndata_into_active_ptr",
+            Name::ZkPtrAddIntoActive => "$zk_ptr_add_into_active",
+            Name::ZkPtrShrinkIntoActive => "$zk_ptr_shrink_into_active",
+            Name::ZkPtrPackIntoActive => "$zk_ptr_pack_into_active",
+
+            Name::ZkMultiplicationHigh => "$zk_multiplication_high",
+
+            Name::ZkGlobalLoad => "$zk_global_load",
+            Name::ZkGlobalExtraAbiData => "$zk_global_extra_abi_data",
+            Name::ZkGlobalStore => "$zk_global_store",
+
+            Name::UserDefined(_) | Name::Verbatim { .. } => {
+                unreachable!()
+            }
+        })
     }
 }
