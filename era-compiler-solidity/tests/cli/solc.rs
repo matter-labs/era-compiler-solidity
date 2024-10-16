a3a7e1d0721a0ec9acd0c22f00eb3bc90ad7a044
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2024-10-16 08:21:44 +0800

    fix: unit tests

--- era-compiler-solidity/tests/cli/solc.rs
+++ era-compiler-solidity/tests/cli/solc.rs
@@ -26,6 +26,24 @@ fn call_zksolc_with_solc_argument() -> anyhow::Result<()> {
 }
 
 #[test]
+fn call_zksolc_without_solc_argument() -> anyhow::Result<()> {
+    let _ = common::setup();
+
+    let mut zksolc = Command::cargo_bin(era_compiler_solidity::DEFAULT_EXECUTABLE_NAME)?;
+
+    let assert = zksolc
+        .arg(cli::TEST_SOLIDITY_CONTRACT_PATH)
+        .env("PATH", "./solc-bin")
+        .assert();
+
+    assert
+        .success()
+        .stderr(predicate::str::contains("Compiler run successful"));
+
+    Ok(())
+}
+
+#[test]
 fn call_zksolc_with_solc_in_standard_json_mode() -> anyhow::Result<()> {
     let _ = common::setup();
 
@@ -59,25 +77,9 @@ fn call_zksolc_without_solc_in_standard_json_mode() -> anyhow::Result<()> {
         .arg(cli::TEST_JSON_CONTRACT_PATH)
         .assert();
 
-    assert.failure().stderr(predicate::str::contains("Error"));
-
-    Ok(())
-}
-
-#[test]
-fn call_zksolc_without_solc_argument() -> anyhow::Result<()> {
-    let _ = common::setup();
-
-    let mut zksolc = Command::cargo_bin(era_compiler_solidity::DEFAULT_EXECUTABLE_NAME)?;
-
-    let assert = zksolc
-        .arg(cli::TEST_SOLIDITY_CONTRACT_PATH)
-        .env("PATH", "./solc-bin")
-        .assert();
-
-    assert
-        .success()
-        .stderr(predicate::str::contains("Compiler run successful"));
+    assert.success().stdout(predicate::str::contains(
+        "The `solc` executable not found in ${PATH}",
+    ));
 
     Ok(())
 }
