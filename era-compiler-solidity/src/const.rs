68d9ab2788f4e6de85c39a7b40fc174cbcc704ff
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-03-28 01:23:15 +0800

    fix: sets the stack limit back to 64 MB

--- era-compiler-solidity/src/const.rs
+++ era-compiler-solidity/src/const.rs
@@ -5,17 +5,8 @@
 /// The default executable name.
 pub static DEFAULT_EXECUTABLE_NAME: &str = "zksolc";
 
-/// The `keccak256` scratch space offset.
-pub const OFFSET_SCRATCH_SPACE: usize = 0;
-
-/// The memory pointer offset.
-pub const OFFSET_MEMORY_POINTER: usize = 2 * era_compiler_common::BYTE_LENGTH_FIELD;
-
-/// The empty slot offset.
-pub const OFFSET_EMPTY_SLOT: usize = 3 * era_compiler_common::BYTE_LENGTH_FIELD;
-
-/// The non-reserved memory offset.
-pub const OFFSET_NON_RESERVED: usize = 4 * era_compiler_common::BYTE_LENGTH_FIELD;
+/// The worker thread stack size.
+pub const WORKER_THREAD_STACK_SIZE: usize = 64 * 1024 * 1024;
 
 ///
 /// The compiler version default function.
